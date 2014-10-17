e175e362d3b2001aea9ec607bc6c2285f03d1a6b
kvaldik <karlvaldimar@gmail.com>
2014-10-17 15:38:17 +0000

    Changed compile tests build script to support @Rule in JUnit

--- bin/compile_tests.bat
+++ bin/compile_tests.bat
@@ -2,4 +2,4 @@ if not exist classes (
     mkdir classes
 )
 
-javac -classpath "classes;lib\junit-4.11.jar" src/test/java/is/kvaldik/stringcalculator/*.java -d classes
+javac -classpath "classes;lib\junit-4.11.jar;lib/hamcrest-core-1.3.jar" src/test/java/is/kvaldik/stringcalculator/*.java -d classes
