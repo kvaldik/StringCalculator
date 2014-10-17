ab54ef490389c7e2eeff18b35725da4e9ee9d679
kvaldik <karlvaldimar@gmail.com>
2014-10-17 14:39:00 +0000

    Refactoring. Added a function to get the delimitter

--- src/main/java/is/kvaldik/stringcalculator/StringCalculator.java
+++ src/main/java/is/kvaldik/stringcalculator/StringCalculator.java
@@ -12,13 +12,13 @@ public class StringCalculator
 			{
 				char delimiter = text.charAt(2);
 				String text2 = text.substring(4);
-				return addNumbers(text2.split(Pattern.quote(String.valueOf(delimiter))));
+				return addNumbers(text2.split(getDelimiter(text)));
 			}
 		if (text == "")
 			return 0;
 		else if (text.contains(",") || text.contains("\n"))
 		{
-			return addNumbers(text.split("[,\n]"));
+			return addNumbers(text.split(getDelimiter(text)));
 		}
 		else
 			return Integer.parseInt(text);
@@ -30,5 +30,21 @@ public class StringCalculator
 		for (String number : numbers)
 			returnValue += Integer.parseInt(number);
 		return returnValue;
+	}
+
+	private static String getDelimiter(String text)
+	{
+		// Check for custom delimiter
+		if (text.length() > 2)
+		{
+			if (text.substring(0,2).equals("//"))
+			{
+				char delimiter = text.charAt(2);
+				return Pattern.quote(String.valueOf(delimiter));
+			}
+		}
+
+		// Else return the default delimiter
+		return "[,\n]";
 	}
 }
